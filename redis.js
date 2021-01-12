c3a580eeabda0bab2ed17fadb2e73bf0f526dc07
Ryan Cummings <73467558+Kindaprogamer@users.noreply.github.com>
2021-01-12 18:25:19 +0000

    FFs work pls

--- redis.js
+++ redis.js
@@ -1,21 +1,36 @@
 const redis = require('redis')
 require('dotenv').config()
-const { redisPath } = require(process.env.REDIS_PATH)
+const redisPath = (process.env.REDIS_PATH)
+
 
 module.exports = async () => {
-    return await new Promise((resolve, reject) => {
-        const client = redis.createClient({
-            url: redisPath
-        })
-
-        client.on('error', (err) => {
-            console.error('redis error ', err)
-            client.quit()
-            reject(err)
-        })
-
-        client.on('ready', () => {
-            resolve(client)
-        })
+  return await new Promise((resolve, reject) => {
+    const client = redis.createClient({
+      url: redisPath,
     })
+
+    client.on('error', (err) => {
+      console.error('Redis error:', err)
+      client.quit()
+      reject(err)
+    })
+
+    client.on('ready', () => {
+      resolve(client)
+    })
+  })
+}
+
+module.exports.expire = (callback) => {
+  const expired = () => {
+    const sub = redis.createClient({ url: redisPath })
+    sub.subscribe('__keyevent@0__:expired', () => {
+      sub.on('message', (channel, message) => {
+        callback(message)
+      })
+    })
+  }
+
+  const pub = redis.createClient({ url: redisPath })
+  pub.send_command('config', ['set', 'notify-keyspace-events', 'Ex'], expired())
 }
