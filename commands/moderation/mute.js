e11a9200b2651e162860439acb215faf2e0e582b
Ryan Cummings <73467558+Kindaprogamer@users.noreply.github.com>
2021-01-12 18:37:26 +0000

    work please

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -7,58 +7,100 @@ module.exports = {
     category: 'Moderation',  
     description: 'to ban a person not following the rules',
     callback: async ({ message, args, text }) => {
-        const { member, channel, content, mentions } = message
-
-        const split = content.split(' ')
-
-        if(split.length !== 4) {
-            channel.send('Please use correct format ' + syntax)
-        }
-
-        const duration = split[2]
-        const durationType = split[3]
-
-        if(isNaN(duration)) {
-            channel.send('Please provide a number for the duration ' + syntax)
-            return
-        }
-
-        const durations = {
-            m: 60,
-            h: 60 * 60,
-            d: 60* 60* 24,
-            perm: -1,
-        }
-
-        if(!durations[durationType]) {
-            channel.send('Please provi;e a valid durationType ' + syntax)
-            return
-        }
-
-        const seconds = duration * durations[durationType]
-
-        const target = message.mentions.users.first()
-
-        if(!target) {
-            channel.send('Please tag a user you would like to mute ' + syntax)
-            return
-        }
-
-        const {id} = target
-
-        const redisClient = await redis()
-        try {
-            const redisKey = `${key}${id}`
-
-            if(seconds > 0) {
-                redisClient.set(redisKey, 'true', 'EX', seconds)
-            } else {
-                redisClient.set(redisKey, 'true')
+        const redisKeyPrefix = 'muted-'
+        
+        redis.expire((message) => {
+            if (message.startsWith(redisKeyPrefix)) {
+              const split = message.split('-')
+        
+              const memberId = split[1]
+              const guildId = split[2]
+        
+              const guild = client.guilds.cache.get(guildId)
+              const member = guild.members.cache.get(memberId)
+        
+              const role = getRole(guild)
+        
+              member.roles.remove(role)
             }
-
-        } finally {
-            redisClient.quit()
-        }
-
+          })
+        
+          const getRole = (guild) => {
+            return guild.roles.cache.find((role) => role.name === 'Muted')
+          }
+        
+          const giveRole = (member) => {
+            const role = getRole(member.guild)
+            if (role) {
+              member.roles.add(role)
+              console.log('Muted ' + member.id)
+            }
+          }
+        
+          command(client, 'mute', async (message) => {
+            // !mute @ duration duration_type
+        
+            const syntax = '[p]mute <target\'s @> <duration as a number> <m, h, d, or life>'
+        
+            const { member, channel, content, mentions, guild } = message
+        
+            const split = content.trim().split(' ')
+        
+            if (split.length !== 4) {
+              channel.send('Please use the correct command syntax: ' + syntax)
+              return
+            }
+        
+            const duration = split[2]
+            const durationType = split[3]
+        
+            if (isNaN(duration)) {
+              channel.send('Please provide a number for the duration. ' + syntax)
+              return
+            }
+        
+            const durations = {
+              m: 60,
+              h: 60 * 60,
+              d: 60 * 60 * 24,
+              life: -1,
+            }
+        
+            if (!durations[durationType]) {
+              channel.send('Please provide a valid duration type. ' + syntax)
+              return
+            }
+        
+            const seconds = duration * durations[durationType]
+        
+            const target = mentions.users.first()
+        
+            if (!target) {
+              channel.send('Please tag a user to mute.')
+              return
+            }
+        
+            const { id } = target
+        
+            console.log('ID:', id)
+        
+            const targetMember = guild.members.cache.get(id)
+            giveRole(targetMember)
+        
+            const redisClient = await redis()
+            try {
+              const redisKey = `${redisKeyPrefix}${id}-${guild.id}`
+        
+              if (seconds > 0) {
+                redisClient.set(redisKey, 'true', 'EX', 10)
+              } else {
+                redisClient.set(redisKey, 'true')
+              }
+            } finally {
+              redisClient.quit()
+            }
+        })
+        
     }
+
 }
